1ce31efd1ac775a9fc686d477da9d7f4b395af8d
Arella Matteo <arella.matteo.95@gmail.com>
2020-06-03 16:58:10 +0200

    added new videos

--- src/app/models/News.ts
+++ src/app/models/News.ts
@@ -15,6 +15,42 @@ export class News {
             subtitle: '&Egrave; disponibile il video su YouTube',
             date: moment(new Date(2020, 1, 7)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=eOWgTYHPUBM'
+        }, 
+        {
+            title: '&laquo;GIUSTIZIA SOCIALE e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 5, 10)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=tbLrAFlAROw'
+        }, 
+        {
+            title: '&laquo;SOLIDARIET&Agrave; e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 5, 12)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=lvvSh0edEwY'
+        }, 
+        {
+            title: '&laquo;MODERAZIONE e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=K-ZVJRnrTiY'
+        }, 
+        {
+            title: '&laquo;SOSTENIBILIT&Agrave; e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=5lBQQNJIHbM'
+        }, 
+        {
+            title: '&laquo;NECESSIT&Agrave; SOCIALI PRIORITARIE e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=NMG7VDp3pO4'
+        }, 
+        {
+            title: '&laquo;FIDUCIA COMUNITARIA e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=KCQjHZIX5UE'
         }
     ];
 }
