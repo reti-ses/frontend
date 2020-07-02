9f7bf037ac3823c52920ad1780230f50532d50ea
comunitaretises <comunitaretises@gmail.com>
2020-07-02 18:03:52 +0200

    added videos

--- src/app/components/progetto/progetto.component.ts
+++ src/app/components/progetto/progetto.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from '../../models/News'
 
 @Component({
   selector: 'app-progetto',
   templateUrl: './progetto.component.html',
-  styleUrls: ['./progetto.component.scss']
+  styleUrls: ['./progetto.component.scss'],
+  providers: [News]
 })
 export class ProgettoComponent implements OnInit {
+
+  news = News.news
 
   constructor() { }
 
--- src/app/models/News.ts
+++ src/app/models/News.ts
@@ -5,52 +5,94 @@ import * as moment from 'moment/moment';
 export class News {
     static news = [
         {
-            title: '&laquo;BENESSERE e Comunit&agrave; Reti SES&raquo;',
+            title: '1 &laquo;BENESSERE e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
             date: moment(new Date(2020, 0, 19)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=8YbBkujwVa0'
         }, 
         {
-            title: '&laquo;BISOGNI UMANI e Comunit&agrave; Reti SES&raquo;',
+            title: '2 &laquo;BISOGNI UMANI e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
             date: moment(new Date(2020, 1, 7)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=eOWgTYHPUBM'
         }, 
         {
-            title: '&laquo;GIUSTIZIA SOCIALE e Comunit&agrave; Reti SES&raquo;',
+            title: '3 &laquo;GIUSTIZIA SOCIALE e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
-            date: moment(new Date(2020, 5, 10)).format('DD-MM-YYYY'),
+            date: moment(new Date(2020, 4, 10)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=tbLrAFlAROw'
         }, 
         {
-            title: '&laquo;SOLIDARIET&Agrave; e Comunit&agrave; Reti SES&raquo;',
+            title: '4 &laquo;SOLIDARIET&Agrave; e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
-            date: moment(new Date(2020, 5, 12)).format('DD-MM-YYYY'),
+            date: moment(new Date(2020, 4, 12)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=lvvSh0edEwY'
         }, 
         {
-            title: '&laquo;MODERAZIONE e Comunit&agrave; Reti SES&raquo;',
+            title: '5 &laquo;MODERAZIONE e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
-            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            date: moment(new Date(2020, 5, 2)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=K-ZVJRnrTiY'
         }, 
         {
-            title: '&laquo;SOSTENIBILIT&Agrave; e Comunit&agrave; Reti SES&raquo;',
+            title: '6 &laquo;SOSTENIBILIT&Agrave; e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
-            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            date: moment(new Date(2020, 5, 2)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=5lBQQNJIHbM'
         }, 
         {
-            title: '&laquo;NECESSIT&Agrave; SOCIALI PRIORITARIE e Comunit&agrave; Reti SES&raquo;',
+            title: '7 &laquo;NECESSIT&Agrave; SOCIALI PRIORITARIE e Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
-            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            date: moment(new Date(2020, 5, 2)).format('DD-MM-YYYY'),
             link: 'https://www.youtube.com/watch?v=NMG7VDp3pO4'
         }, 
         {
-            title: '&laquo;FIDUCIA COMUNITARIA e Comunit&agrave; Reti SES&raquo;',
+            title: '8 &laquo;FIDUCIA COMUNITARIA e Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 5, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=KCQjHZIX5UE'
+        },
+        {
+            title: '9 &laquo;Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 5, 29)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=MAl9ZsMlj1E'
+        }, 
+        {
+            title: '10 &laquo;BENEFICIARI Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 5, 29)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=p_jJqAf3f88'
+        }, 
+        {
+            title: '11 &laquo;ATTIVIT&Agrave; Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 1)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=NLheWCL8GjM'
+        }, 
+        {
+            title: '12 &laquo;SISTEMA ECONOMICO Comunit&agrave; Reti SES&raquo;',
             subtitle: '&Egrave; disponibile il video su YouTube',
             date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
-            link: 'https://www.youtube.com/watch?v=KCQjHZIX5UE'
+            link: 'https://www.youtube.com/watch?v=0fqWaOpkQL0'
+        }, 
+        {
+            title: '13 &laquo;GUADAGNI E PREVIDENZA Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=8uc1nF2S_ZI'
+        }, 
+        {
+            title: '14 &laquo;FINANZIAMENTI Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=LKcXWVUHft8'
+        },
+        {
+            title: '15 &laquo;BELLEZZA Comunit&agrave; Reti SES&raquo;',
+            subtitle: '&Egrave; disponibile il video su YouTube',
+            date: moment(new Date(2020, 6, 2)).format('DD-MM-YYYY'),
+            link: 'https://www.youtube.com/watch?v=vAo0OgBWeaI'
         }
     ];
 }
